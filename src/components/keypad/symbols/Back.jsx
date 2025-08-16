b34ee111e295da3bbdf2e310e9a85b52982e3a86
Utplaksh Mishra <umishra8770@gmail.com>
2025-08-16 12:25:29 +0530

    added delete function to codebase that will delete no. one by one on click of back btn

--- src/components/keypad/symbols/Back.jsx
+++ src/components/keypad/symbols/Back.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import backImg from "../../../assets/pngwing.com.png";
+import { CalculateContest } from "../../../context/calc";
 
 const Back = () => {
+  const value = useContext(CalculateContest);
+  const setNumber = value.setNumber;
+  const backBtn = () => {
+    setNumber((prevs) => {
+      const lastElement = prevs[prevs.length - 1];
+      return prevs.filter((prev) => prev != lastElement);
+    });
+  };
   return (
-    <div className="bg-red-600 hover:bg-[#990000] flex justify-center items-center rounded-2xl">
-      <button>
-        <img src={backImg} className="w-[45px] invert" alt="back" />
-      </button>
-    </div>
+    <button
+      className="w-full h-full flex justify-center items-center bg-red-600 hover:bg-[#990000] rounded-2xl"
+      onClick={backBtn}
+    >
+      <img src={backImg} className="w-[45px] invert" alt="back" />
+    </button>
   );
 };
 
