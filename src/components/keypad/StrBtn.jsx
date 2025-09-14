274419bb1a81a949249e42e9e968ebaa7301d486
Utplaksh Mishra <umishra8770@gmail.com>
2025-09-14 10:41:18 +0530

    added styles to keypad and learnt new way to write multiple style class of tailwind in className

--- src/components/keypad/StrBtn.jsx
+++ src/components/keypad/StrBtn.jsx
@@ -4,13 +4,20 @@ import operator from "../../modules/handleOperator";
 
 const StrBtn = ({ btn }) => {
   const value = useContext(OperationContext);
+  const baseStyleClass = "border border-gray-950 hover:bg-gray-600";
+  const gridSpanClass =
+    btn === "AC" || btn === "=" ? "col-span-2 font-bold" : "";
+  let activeClass;
+  if (btn === "AC") {
+    activeClass = "active:bg-red-500 transition-colors";
+  } else if (btn === "=") {
+    activeClass = "active:bg-green-500 transition-colors";
+  } else {
+    activeClass = "active:bg-gray-700";
+  }
   return (
     <button
-      className={
-        btn == "AC" || btn == "="
-          ? "col-span-2 border border-gray-950 font-bold  hover:bg-gray-600"
-          : "border border-gray-950  hover:bg-gray-600"
-      }
+      className={`${baseStyleClass} ${gridSpanClass}  ${activeClass}`}
       onClick={() => {
         operator(value.dispatch, btn);
       }}
